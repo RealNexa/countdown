f4cbf6d5aecfd8dc9bd7f47997851091bc878375
RealNexa <39256083+RealNexa@users.noreply.github.com>
2019-04-29 19:40:26 +0200

    Update displayTime.py

--- displayTime.py
+++ displayTime.py
@@ -2,12 +2,17 @@ import datetime
 import time
 import os
 
+yearGoal = 2019
+monthGoal = 6
+dayGoal = 4
+
+
 def update():
     today = datetime.date.today()
     dt  = datetime.datetime
     now = dt.now()
     #insert the date under V here V you want to count to.
-    daysAndTime = str(dt(2019, 6, 4) - dt(year=now.year, month=now.month, day=now.day, minute=now.minute, second=now.second)).split()
+    daysAndTime = str(dt(yearGoal, monthGoal, dayGoal) - dt(year=now.year, month=now.month, day=now.day, hour=now.hour, minute=now.minute, second=now.second)).split()
 
     days = daysAndTime[0]
 
@@ -22,10 +27,12 @@ def update():
     totalminutes = totalseconds / 60
 
     totalhours = totalminutes / 60
-    
+
+    procentage = 100 * (1-(totalseconds/3039900))
+   
 
     
-    return "Countdown Timer created by Ulf Olsen\n¤-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-¤\n \nCurrent time: {}\n\nCounting down to 2019 - 6 - 4\nCurrent date: {} - {} - {} \n\nTimeleft: {} : {} : {} : {}\nTotal Weeks {} \nTotal Hours: {} \nTotal Minutes: {} \nTotal Seconds: {}".format(time.strftime('%H:%M:%S',time.localtime()), today.year, today.month, today.day, days, hours, minutes, seconds,(((int(totalseconds)/60)/24)/7)/60 ,int(totalhours), int(totalminutes), totalseconds)
+    return "Countdown Timer created by Ulf Olsen\n¤-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-¤\nCurrent time: {}\nCurrent date: {} - {} - {}\n\nCounting down to: {} - {} - {} \n\nTimeleft: {} : {} : {} : {}\nTotal Weeks {} \nTotal Hours: {} \nTotal Minutes: {} \nTotal Seconds: {} \nPercent(From 19-04-29): {}%".format(time.strftime('%H:%M:%S',time.localtime()), today.year, today.month, today.day, yearGoal, monthGoal, dayGoal, days, hours, minutes, seconds,(((int(totalseconds)/60)/24)/7)/60 ,int(totalhours), int(totalminutes), totalseconds, procentage)
 
 while True:
     
