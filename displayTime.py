2db0ca0609c7656c8345802d4d42fc32da2aa95e
RealNexa <39256083+RealNexa@users.noreply.github.com>
2019-05-12 03:11:20 +0200

    Update displayTime.py

--- displayTime.py
+++ displayTime.py
@@ -2,44 +2,60 @@ import datetime
 import time
 import os
 
-yearGoal = 2019
-monthGoal = 6
-dayGoal = 4
+#insert the date under you want to count to.
+yearGoal = 2019 #Year
+monthGoal = 6   #Month
+dayGoal = 4     #Day
+hourGoal = 0   #Hour
+minuteGoal = 0 #Minute
+secondGoal = 0   #Second
 
+startup = True
+
+first  = datetime.datetime
+fnow = first.now()
 
 def update():
+    isDay = False
     today = datetime.date.today()
     dt  = datetime.datetime
     now = dt.now()
-    #insert the date under V here V you want to count to.
-    daysAndTime = str(dt(yearGoal, monthGoal, dayGoal) - dt(year=now.year, month=now.month, day=now.day, hour=now.hour, minute=now.minute, second=now.second)).split()
 
-    days = daysAndTime[0]
+    daysAndTime = str(dt(yearGoal, monthGoal, dayGoal, hourGoal, minuteGoal, secondGoal) - dt(year=now.year, month=now.month, day=now.day, hour=now.hour, minute=now.minute, second=now.second))
 
-    theThree = daysAndTime[2].split(":")
-
-    hours = theThree[0]
-    minutes = theThree[1]
-    seconds = theThree[2]
-
+    if "day" in daysAndTime:
+        days = daysAndTime.split()[0]
+        hours = daysAndTime.split()[2].split(":")[0]
+        minutes = daysAndTime.split()[2].split(":")[1]
+        seconds = daysAndTime.split()[2].split(":")[2]
+    else: 
+        days = 0
+        hours = daysAndTime.split(":")[0]
+        minutes = daysAndTime.split(":")[1]
+        seconds = daysAndTime.split(":")[2]
+        
     totalseconds = int(seconds) + int(minutes) * 60 + int(hours) * 60 * 60 + int(days) * 24 * 60 * 60
+    
+    if startup:
+        return totalseconds
 
     totalminutes = totalseconds / 60
 
     totalhours = totalminutes / 60
 
-    procentage = 100 * (1-(totalseconds/3039900))
-   
+    procentage = 100 * (1-(totalseconds/startValue))
+
+
+    return "Date: " + str(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())) + "\nCounting to: " + str(yearGoal) + "-" + str(monthGoal) + "-" + str(dayGoal) + " " + str(hourGoal).zfill(2) + ":" + str(minuteGoal).zfill(2) + ":" + str(secondGoal).zfill(2) + "\n\nTime Left: " + str(days) + ":" + str(hours) + ":" + str(minutes) + ":" + str(seconds) + "\nWeeks Left: " + str((((int(totalseconds)/60)/24)/7)/60) + "\nTotal Hours: " + str(int(totalhours)) + "\nTotal Minutes: " + str(int(totalminutes)) + "\nTotal Seconds: " + str(int(totalseconds)) + "\n\nPercent" + "(" + str(fnow.year) + "-" + str(fnow.month) + "-" + str(fnow.day) + "): \n" + str(procentage) + "%"
+
 
     
-    return "Countdown Timer created by Ulf Olsen\n¤-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-¤\nCurrent time: {}\nCurrent date: {} - {} - {}\n\nCounting down to: {} - {} - {} \n\nTimeleft: {} : {} : {} : {}\nTotal Weeks {} \nTotal Hours: {} \nTotal Minutes: {} \nTotal Seconds: {} \nPercent(From 19-04-29): {}%".format(time.strftime('%H:%M:%S',time.localtime()), today.year, today.month, today.day, yearGoal, monthGoal, dayGoal, days, hours, minutes, seconds,(((int(totalseconds)/60)/24)/7)/60 ,int(totalhours), int(totalminutes), totalseconds, procentage)
-
 while True:
-    
-    x = update()
+    if startup:
+        startValue = update()
+        startup = False
     os.system("clear")
-    print(x)
+    print(update())
     time.sleep(1)
-
-
+    
 
